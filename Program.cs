8fabe4173f5caaf37afbebdea4321b1ca51eee3f
William Pfaffe <william.pfaffe@gmail.com>
2018-11-19 17:08:58 +0100

    Start

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+
+    public static string startPeg, endPeg, tempPeg;
+    public static int totalDisks;
+
+    public static void Main(String[] args)
+    {
+        startPeg = "A";
+        endPeg = "C";
+        tempPeg = "B";
+        totalDisks = 3;
+
+        solver(totalDisks, startPeg, endPeg, tempPeg);
+        Console.WriteLine("#########################");
+    }
+
+    private static void solver(int n, string startPeg, string endPeg, string tempPeg)
+    {
+        if (n > 0)
+        {
+            solver(n - 1, startPeg, tempPeg, endPeg);
+            Console.WriteLine("#########################");
+            Console.WriteLine("Moving disk from " + startPeg + " to " + endPeg);
+            solver(n - 1, tempPeg, endPeg, startPeg);
+        }
+    }
+
+}
